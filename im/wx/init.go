68082b803c888143496a4afc30dc9bd88b8a3e0d
EvanDbg <yangf1023@icloud.com>
2021-11-03 22:01:36 +0800

    Update init.go

--- im/wx/init.go
+++ im/wx/init.go
@@ -175,7 +175,7 @@ func init() {
 			DeviceName: "EVAN's iPhone12",
 			Title: pusherTitle,
 			Subtitle: "",
-			Message: fmt.Println("string", jms.Msg.(string)),
+			Message: fmt.Sprintf("%s", jms.Msg),
 		}
 		apikey := wx.Get("apikey")
 		dbCode := wx.Get("dbCode")
